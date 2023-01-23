64456b890c99f1beaa7c6e9e7dbfcc9947049e9b
Rosivaldo Lucas <rosivaldo@newsoft.online>
2023-01-23 19:09:10 -0300

    monolito-modular - Inicio do projeto que visa construir uma estrutura de projeto de monolito modular.

---
+++ src/main/java/com/rosivaldolucas/fullcycle/monolitomodular/MonolitoModularApplication.java
@@ -0,0 +1,13 @@
+package com.rosivaldolucas.fullcycle.monolitomodular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolitoModularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolitoModularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rosivaldolucas/fullcycle/monolitomodular/MonolitoModularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosivaldolucas.fullcycle.monolitomodular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolitoModularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
